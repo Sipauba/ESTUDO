911b5a3c27eda99cd5e9708a22da612667c50fcb
Mateus de Souza Sipauba <98923954+Sipauba@users.noreply.github.com>
2023-06-01 22:33:52 -0300

    A rotina já está com a interface quase pronta. Ainda falta corrigir o problema dos rádios buttons e definir toda a lógica

--- ensaio-nova-rotina-winthor/rotina.py
+++ ensaio-nova-rotina-winthor/rotina.py
@@ -1,7 +1,9 @@
 from tkinter import *
 from tkinter import ttk
 import mysql.connector
+from tkcalendar import DateEntry
 
+"""
 # Variável que irá conectar a aplicação com o banco de dados
 conecta_banco = mysql.connector.connect(
     host='192.168.40.112',
@@ -27,14 +29,70 @@ def consultar():
     cursor.close()
     conecta_banco.close()
 
+"""
 
 root = Tk()
-#root.geometry('300x200')
+root.geometry('800x500')
+root.title('Status Req. Mat. Consumo')
 
-frame = Frame(root)
 
-btn = Button(root,text='Consultar',command=consultar)
-btn.pack()
+
+#----------------------------------------------
+
+frame_cabecalho = Frame(root,
+              width=640,
+              height=110,
+              relief=SOLID,
+              bd=1,
+              )
+frame_cabecalho.pack(pady=(50,10))
+
+frame_radio = Frame(root)
+frame_radio.pack()
+
+#-------------------------------------------------------------
+
+label_filial = Label(frame_cabecalho, text='Filial')
+label_filial.grid(row=0, column=0, pady=(20,0))
+
+campo_filial = ttk.Combobox(frame_cabecalho, width=4, values=['1','3','4','5','6','17','18','19','20','61','70','99'])
+campo_filial.grid(row=1, column=0, pady=(0,20), padx=(20,0))
+
+label_num_req = Label(frame_cabecalho, text='Nº Requisição')
+label_num_req.grid(row=0, column=1, pady=(20,0))
+
+campo_requisicao = Entry(frame_cabecalho)
+campo_requisicao.grid(row=1, column=1, pady=(0,20), padx=(20,0))
+
+label_data_ini = Label(frame_cabecalho, text='Data Inicial')
+label_data_ini.grid(row=0, column=2, pady=(20,0))
+
+data_inicial = DateEntry(frame_cabecalho, date_pattern='dd/mm/yyyy')
+data_inicial.grid(row=1, column=2, pady=(0,20), padx=(20,0))
+
+label_data_fin = Label(frame_cabecalho, text='Data Final')
+label_data_fin.grid(row=0,column=3, pady=(20,0))
+
+data_final = DateEntry(frame_cabecalho, date_pattern='dd/mm/yyyy')
+data_final.grid(row=1,column=3,  pady=(0,20), padx=(20,20))
+
+#-------------------------------------------------------------------
+
+valor_rad_aprov = IntVar()
+valor_rad_cancel = IntVar()
+valor_rad_todos = IntVar()
+
+radio_aprov = Radiobutton(frame_radio,text='Aprovados', variable = valor_rad_aprov, value=1, indicatoron=1)
+radio_aprov.grid(row=0, column=0)
+
+radio_cancel = Radiobutton(frame_radio,text='Cancelados', variable = valor_rad_cancel, value=2, indicatoron=1)
+radio_cancel.grid(row=0,column=1)
+
+radio_todos = Radiobutton(frame_radio,text='Todos', variable = valor_rad_todos, value=3, indicatoron=1)
+radio_todos.grid(row=0, column=2)
+
+btn = Button(root,text='Pesquisar')
+btn.pack(pady=(20,30))
 
 # Define o a quantidade de colunas
 tree = ttk.Treeview(root, columns=('coluna1','coluna2','coluna3'))
