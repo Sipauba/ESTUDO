8bc4fc2ce3fc50e1baf15486ae155a5133ef3936
Mateus de Souza Sipauba <mateus.sipauba@sodinenet.com.br>
2023-06-06 17:29:57 -0300

    Rotina totalmente funcional(aparentemente). Comandos SQL funcionando normalmente, sem problemas nas datas. botões rádios operando. Preciso incluir o botão para buscar as requisições que estão pendentes

--- ensaio-nova-rotina-winthor/rotina.py
+++ ensaio-nova-rotina-winthor/rotina.py
@@ -21,25 +21,31 @@ cursor = conecta_banco.cursor()
 
 # Esta função executa a consulta que irá preencher a treeview com as informações sobre arequisição
 def consultar():
-
+    # Recebe os valores dos campos e os coloca em variáveis que serão posteriormente tratadas para se adequar ao formato do banco de dados (os campos de data têm essa peculiaridade)
     filial = campo_filial.get()
     num_req = campo_requisicao.get()
     data_ini = data_inicial.get_date()
     data_fin = data_final.get_date()
 
-    consulta = 'SELECT NUMPREREQUISICAO, CODFILIAL, DATA, CODFUNCREQ, MOTIVO, SITUACAO, NUMTRANSVENDA FROM PCPREREQMATCONSUMOC WHERE CODFILIAL = {}'.format(filial)
+    #consulta = 'SELECT NUMPREREQUISICAO, CODFILIAL, DATA, CODFUNCREQ, MOTIVO, SITUACAO, NUMTRANSVENDA FROM PCPREREQMATCONSUMOC'
+    consulta = 'SELECT NUMPREREQUISICAO, CODFILIAL, DATA, CODFUNCREQ, MOTIVO, SITUACAO, NUMTRANSVENDA FROM PCPREREQMATCONSUMOC WHERE CODFILIAL IN {}'.format(filial)
     
     # Estas condições irão concatenar com o valor da variável 'consulta' dependendo se será fornecido o número da requisição ou as datas inicial e final
+    if filial == '':
+        consulta += '(1, 3, 4, 5, 6, 7, 17 ,18 ,19 ,20 ,61 ,70 , 99)'
     if num_req:
         consulta += 'AND NUMPREREQUISICAO = {}'.format(num_req)
     if data_ini and data_fin:
+        # Este trecho vai receber o valor das variáveis que receberam os valores da DateEntry e formatar de uma forma que seja possível fazer a consulta SQL no banco oracle
         data_ini = data_ini.strftime('%d-%b-%Y')
         data_fin = data_fin.strftime('%d-%b-%Y')
         consulta += " AND DATA BETWEEN TO_DATE('{}', 'DD-MON-YYYY') AND TO_DATE('{}', 'DD-MON-YYYY')".format(data_ini, data_fin)
-    """
-    if data_ini and data_final:
-        consulta += 'AND DATA BETWEEN {} AND {}'.format(data_ini, data_fin)
-    """
+    if valor_radio.get() == 1:
+        consulta += "AND SITUACAO = 'A'"
+    if valor_radio.get() == 2:
+        consulta += "AND SITUACAO = 'C'"
+        
+
     # Executa a consulta
     cursor.execute(consulta)
 
@@ -69,11 +75,13 @@ frame_cabecalho.pack(pady=(50,10))
 
 #-------------------------------------------------------------
 
+# Cria todos os widgets necessários para captar os dados
 label_filial = Label(frame_cabecalho, text='Filial')
 label_filial.grid(row=0, column=0, pady=(20,0))
 
-campo_filial = ttk.Combobox(frame_cabecalho, width=4, values=['1','3','4','5','6','17','18','19','20','61','70','99'])
-campo_filial.current(4)
+campo_filial = ttk.Combobox(frame_cabecalho, width=4, values=['','1','3','4','5','6','7','17','18','19','20','61','70','99'])
+# Mantém a filial 6 ao iniciar o programa
+#campo_filial.current(4)
 campo_filial.grid(row=1, column=0, pady=(0,20), padx=(20,0))
 
 label_num_req = Label(frame_cabecalho, text='Nº Requisição')
@@ -86,14 +94,12 @@ label_data_ini = Label(frame_cabecalho, text='Data Inicial')
 label_data_ini.grid(row=0, column=2, pady=(20,0))
 
 data_inicial = DateEntry(frame_cabecalho, date_pattern='dd/mm/yyyy')
-data_inicial.set_date(None)
 data_inicial.grid(row=1, column=2, pady=(0,20), padx=(20,0))
 
 label_data_fin = Label(frame_cabecalho, text='Data Final')
 label_data_fin.grid(row=0,column=3, pady=(20,0))
 
 data_final = DateEntry(frame_cabecalho, date_pattern='dd/mm/yyyy')
-data_final.set_date(None)
 data_final.grid(row=1,column=3,  pady=(0,20), padx=(20,20))
 
 #-------------------------------------------------------------------
@@ -104,22 +110,13 @@ frame_radio.pack()
 
 valor_radio = IntVar()
 
-def aprovados():
-    print('Aprovados')
-    
-def cancelados():
-    print('Cancelados')
-
-def todos():
-    print('Todos')
-
-radio_aprov = Radiobutton(frame_radio,text='Aprovados', variable = valor_radio, value=1, command=aprovados, indicatoron=1)
+radio_aprov = Radiobutton(frame_radio,text='Aprovados', variable = valor_radio, value=1, indicatoron=1)
 radio_aprov.grid(row=0, column=0)
 
-radio_cancel = Radiobutton(frame_radio,text='Cancelados', variable = valor_radio, value=2, command=cancelados, indicatoron=1)
+radio_cancel = Radiobutton(frame_radio,text='Cancelados', variable = valor_radio, value=2, indicatoron=1)
 radio_cancel.grid(row=0,column=1)
 
-radio_todos = Radiobutton(frame_radio,text='Todos', variable = valor_radio, value=3, command=todos, indicatoron=1)
+radio_todos = Radiobutton(frame_radio,text='Todos', variable = valor_radio, value=3, indicatoron=1)
 radio_todos.grid(row=0, column=2)
 
 radio_todos.select()
@@ -127,8 +124,15 @@ radio_todos.select()
 btn = Button(root, text='Pesquisar', command=consultar)
 btn.pack(pady=(20,30))
 
+#------------------------------------------------------------------------------------------------------------------------------------
+
 # Define o a quantidade de colunas
 tree = ttk.Treeview(root, columns=('coluna1','coluna2','coluna3','coluna4','coluna5','coluna6','coluna7'))
+
+# Centraliza o conteúdo de todas as colunas
+for coluna in ('coluna1','coluna2','coluna3','coluna4','coluna5','coluna6','coluna7'):
+    tree.column(coluna, anchor='center')
+
 
 # Nomeia o cabeçalho das colunas
 tree.heading('coluna1',text='NUMREQ')
@@ -139,6 +143,7 @@ tree.heading('coluna5',text='MOTIVO')
 tree.heading('coluna6',text='SITUACAO')
 tree.heading('coluna7',text='TRANSVENDA')
 
+# Define uma largura pardrão para cada coluna, pode ser expandida normalmente clicando e arrastando
 tree.column('coluna1', width=60)
 tree.column('coluna2', width=40)
 tree.column('coluna3', width=90)
@@ -160,4 +165,3 @@ height = 200
 tree.place(x=x, y=y, width=width, height=height)
 
 root.mainloop()
-#
